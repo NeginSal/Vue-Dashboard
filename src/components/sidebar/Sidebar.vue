bd39c902d158fb8e9139c83615f677a8a9ce36ee
NeginSal <neginsaljooghi@gmail.com>
2024-09-03 17:54:47 +0330

    delete components & add sidebar

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-primary">
+    this is sidebar
+    <button @click="toggleCollapse" class="btn btn-sm rounded-circle border">
+      <i class="bi bi-arrow-left-right fs-5"></i>
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  toggleCollapse: {
+    type: Function,
+    default: false,
+  },
+})
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Sidebar from '@/components/sidebar/Sidebar.vue';
+import { ref } from 'vue'
+const collapse = ref(false);
+
+const toggleCollapse = () => {
+  collapse.value = !collapse.value;
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container-fluid">
+    <div class="row">
+      <div :class="collapse ? 'col-lg-1' : 'col-lg-2' ">
+        <Sidebar :toggleCollapse="toggleCollapse"/>
+      </div>
+      <div class="col-lg-10 bg-danger">dddddddd</div>
+    </div>
+  </div>
 </template>
